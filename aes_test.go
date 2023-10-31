7e85308f455ae09e12fd95f3ca64e1e30e7e74e6
shenghui <shenghui0779@gmail.com>
2023-10-31 11:38:53 +0800

    Update test

--- aes_test.go
+++ aes_test.go
@@ -11,11 +11,11 @@ import (
 func TestAesCBC(t *testing.T) {
 	key := "AES256Key-32Characters1234567890"
 	iv := key[:16]
-	data := "IloveYiigo"
+	data := "ILoveYiigo"
 
 	cipher, err := AESEncryptCBC([]byte(key), []byte(iv), []byte(data))
 	assert.Nil(t, err)
-	assert.Equal(t, "4gxKE4uo0MNIe3TY+LOT2CqDKi9aBQ48KOTKjZDJnEs=", cipher.String())
+	assert.Equal(t, "hSXsKUV2fbG8F2JlVcnra876xvKxyXwoJvaebTtWGzQ=", cipher.String())
 
 	plain, err := AESDecryptCBC([]byte(key), []byte(iv), cipher.Bytes())
 	assert.Nil(t, err)
@@ -24,11 +24,11 @@ func TestAesCBC(t *testing.T) {
 
 func TestAesECB(t *testing.T) {
 	key := "AES256Key-32Characters1234567890"
-	data := "IloveYiigo"
+	data := "ILoveYiigo"
 
 	cipher, err := AESEncryptECB([]byte(key), []byte(data))
 	assert.Nil(t, err)
-	assert.Equal(t, "aXvKx3jVNrUWzzHBI+az+rpl6eN3wP/L8phJTP4aWFE=", cipher.String())
+	assert.Equal(t, "FqrgSRCY4zBRYBOg4Pe3Vbpl6eN3wP/L8phJTP4aWFE=", cipher.String())
 
 	plain, err := AESDecryptECB([]byte(key), cipher.Bytes())
 	assert.Nil(t, err)
@@ -38,14 +38,14 @@ func TestAesECB(t *testing.T) {
 func TestAesGCM(t *testing.T) {
 	key := "AES256Key-32Characters1234567890"
 	nonce := key[:12]
-	data := "IloveYiigo"
+	data := "ILoveYiigo"
 	aad := "IIInsomnia"
 
 	cipher, err := AESEncryptGCM([]byte(key), []byte(nonce), []byte(data), []byte(aad))
 	assert.Nil(t, err)
-	assert.Equal(t, "qeiumnRZKY42HZjRPuwOm7wTdj/FKddd5uI=", cipher.String())
-	assert.Equal(t, "qeiumnRZKY42HQ==", base64.StdEncoding.EncodeToString(cipher.Data()))
-	assert.Equal(t, "mNE+7A6bvBN2P8Up113m4g==", base64.StdEncoding.EncodeToString(cipher.Tag()))
+	assert.Equal(t, "qciumnRZKY42HVjng/cUjd0V+OJZB6ZwRF8=", cipher.String())
+	assert.Equal(t, "qciumnRZKY42HQ==", base64.StdEncoding.EncodeToString(cipher.Data()))
+	assert.Equal(t, "WOeD9xSN3RX44lkHpnBEXw==", base64.StdEncoding.EncodeToString(cipher.Tag()))
 
 	plain, err := AESDecryptGCM([]byte(key), []byte(nonce), cipher.Bytes(), []byte(aad))
 	assert.Nil(t, err)
